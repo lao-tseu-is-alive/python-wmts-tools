f387358de6cf4384f238931af772fbf3f3741975
cgil <lao.tseu.is.alive@gmail.com>
2025-01-16 16:04:27 +0100

    added annotated params

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
+from typing import Annotated
+
 from fastapi import FastAPI, status, HTTPException
+
 from app.wmts.swisstopogrid28 import SwissTopoGrid28
 
 app = FastAPI()
@@ -6,11 +9,16 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"docs": "World"}
+    return {"docs_url": "/docs", "redoc_url": "/redoc"}
 
 
 @app.get("/tiles/{zoom}/{col}/{row}")
-def read_tiles(zoom: int, col: int, row: int, q: str = None):
+def read_tiles(
+        zoom: Annotated[int, "Zoom level"],
+        col: Annotated[int, "Tile Column"],
+        row: Annotated[int, "Tile Row"],
+        q: str | None = None
+):
     ch_grid = SwissTopoGrid28()
     try:
         bbox = ch_grid.get_tile_bbox(zoom, col, row)
